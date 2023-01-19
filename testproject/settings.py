ea2cef3db1c78a74b33a0593510cd2421605ed7a
abdoohossamm <abdoohossamm@outlook.com>
2023-01-19 20:27:40 +0200

    Fix: fix the project directory

--- testproject/settings.py
+++ testproject/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'testproject.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'testproject.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
